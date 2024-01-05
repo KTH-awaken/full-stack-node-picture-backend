7d3f050dc21d86acb5dbe490f5e7fa2469f3027f
Hamada Aljarrah <hamadasepost@gmail.com>
2024-01-05 14:13:04 +0100

    docker

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 const multer = require('multer');
 const express = require('express');
-const cors = require('cors'); 
+const cors = require('cors');
 const { format } = require('date-fns');
 
 
@@ -19,7 +19,7 @@ const replace = multer({ storage: storage });
 
 // MySQL config
 const pool = mysql.createPool({
-  // host: 'localhost',fungerar när node inte va i container
+  // host: 'localhost', //fungerar när node inte va i container
   host: 'mysql',// namn på db service
   user: 'root',
   password: 'root',
@@ -43,9 +43,9 @@ app.get('/api/pictures', (req, res) => {
 //Get pictures by patient email
 app.get('/api/get-pictures/:patientEmail', (req, res) => {
   const patientEmail = req.params.patientEmail;
-
   pool.query('SELECT * FROM pictures WHERE patientEmail = ?', [patientEmail], (error, results, fields) => {
     if (error) {
+      console.trace(error);
       return res.status(500).json({ error: 'Failed to fetch pictures from the database' });
     }
 
@@ -60,28 +60,28 @@ app.post('/api/upload-picture', upload.single('picture'), (req, res) => {
   const patientEmail = req.body.patientEmail;
   const doctorEmail = req.body.doctorEmail;
   const date = format(new Date(req.body.date), 'yyyy-MM-dd HH:mm:ss');
-  console.log(req)
-  console.log(req.body)
+  console.log({ picture_data_base64: req.body.picture_data_base64 ? " image exist" : "not image" })
+
+
 
   pool.query('INSERT INTO pictures (picture_data_base64, patientEmail, doctorEmail, date) VALUES (?, ?, ?, ?)', [picture_data_base64, patientEmail, doctorEmail, date], (error, results, fields) => {
     if (error) {
-      console.log(error)
       return res.status(500).json({ error: 'Failed to insert picture into the database' });
     }
-  
+
     res.json({ success: true });
   });
 });
 
 //POST REPLACE
 app.post('/api/replace-picture', replace.single('picture'), (req, res) => {
-    const id = req.body.id;
-    const picture_data_base64 = req.body.picture_data_base64;
-    const patientEmail = req.body.patientEmail;
-    const doctorEmail = req.body.doctorEmail;
-    const date = format(new Date(req.body.date), 'yyyy-MM-dd HH:mm:ss');
+  const id = req.body.id;
+  const picture_data_base64 = req.body.picture_data_base64;
+  const patientEmail = req.body.patientEmail;
+  const doctorEmail = req.body.doctorEmail;
+  const date = format(new Date(req.body.date), 'yyyy-MM-dd HH:mm:ss');
 
-    pool.query('UPDATE db.pictures SET picture_data_base64 = ?, patientEmail = ?, doctorEmail = ?, date = ? WHERE id = ?',
+  pool.query('UPDATE db.pictures SET picture_data_base64 = ?, patientEmail = ?, doctorEmail = ?, date = ? WHERE id = ?',
     [picture_data_base64, patientEmail, doctorEmail, date, id],
     (error, results, fields) => {
       if (error) {
@@ -95,25 +95,24 @@ app.post('/api/replace-picture', replace.single('picture'), (req, res) => {
 });
 
 
-  const PORT = process.env.PORT || 8000;
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
-  
-  
-  app.get('/test-db-connection', (req, res) => {
-    console.log('in test connection')
-    pool.query('SELECT 1 + 1 AS result', (error, results, fields) => {
-      if (error) {
-        return res.status(500).json({ error: 'Database Error' });
-      }
-      
-      
-      res.json({ result: results[0].result });
-    });
-  });
-  
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
-  
-  
-  
+
+app.get('/test-db-connection', (req, res) => {
+  console.log('in test connection')
+  pool.query('SELECT 1 + 1 AS result', (error, results, fields) => {
+    if (error) {
+      return res.status(500).json({ error: 'Database Error' });
+    }
+
+
+    res.json({ result: results[0].result });
+  });
+});
+
+
+
+
